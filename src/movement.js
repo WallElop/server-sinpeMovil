b12636ec288d075d038d00a2d9a1950eb5341750
Walter <wallyjosuelopeza@gmail.com>
2022-12-14 21:29:45 -0600

    Get movement by keys and get last movements with limit

--- src/movement.js
+++ src/movement.js
@@ -6,9 +6,11 @@ const createMovement = async (event) => {
   try {
     const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-    const { senderNumber, amount, receiverNumber, detail } = JSON.parse(event.body);
+    const { senderNumber, amount, receiverNumber, detail } = JSON.parse(
+      event.body
+    );
     const createdAt = new Date().toISOString();
-    
+
     const newMovement = {
       senderNumber,
       createdAt,
@@ -16,7 +18,7 @@ const createMovement = async (event) => {
       receiverNumber,
       detail,
     };
-    
+
     await dynamoDb
       .put({
         TableName: "MovementTable",
@@ -41,13 +43,28 @@ const createMovement = async (event) => {
 const getMovement = async (event) => {
   try {
     const dynamoDb = new AWS.DynamoDB.DocumentClient();
+    const { createdAt } = JSON.parse(event.body);
+    const senderNumber = event.pathParameters.number;
 
-    /**
-     * TODO: Get the movement from the database using the senderNumber and createdAt
-     */
+    const params = {
+      TableName: "MovementTable",
+      ExpressionAttributeValues: {
+        ":senderNumber": senderNumber,
+        ":createdAt": createdAt,
+      },
+      KeyConditionExpression:
+        "senderNumber = :senderNumber AND createdAt = :createdAt",
+    };
+
+    const result = await dynamoDb.query(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
   } catch (error) {
     return {
-      statusCode: 400,
+      statusCode: 404,
       body: {
         message: error.message,
       },
@@ -55,7 +72,46 @@ const getMovement = async (event) => {
   }
 };
 
+const getMovements = async (event) => {
+  // try {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+  const { number } = event.pathParameters;
+  const { createdAt } = JSON.parse(event.body);
+
+  const params = {
+    TableName: "MovementTable",
+    ExpressionAttributeValues: {
+      ":senderNumber": number,
+    },
+    FilterExpression:
+      "senderNumber = :senderNumber OR receiverNumber = :senderNumber",
+    Limit: 10,
+    ExclusiveStartKey: createdAt
+      ? {
+          senderNumber: number,
+          createdAt: createdAt,
+        }
+      : null,
+  };
+
+  const movements = await dynamoDb.scan(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(movements),
+  };
+  // } catch (error) {
+  //   return {
+  //     statusCode: 404,
+  //     body: {
+  //       message: error.message,
+  //     },
+  //   };
+  // }
+};
 
 module.exports = {
   createMovement,
+  getMovement,
+  getMovements,
 };
